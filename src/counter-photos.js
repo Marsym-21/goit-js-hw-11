f350e258e16bdc37e7fd801d050e1ca1dec50b2d
Marsym-21 <mini.garnett.21@gmail.com>
2023-03-20 15:53:09 +0100

    123

--- src/counter-photos.js
+++ src/counter-photos.js
@@ -1,5 +1,9 @@
 function counterPhotos(photo) {
   console.log(photo.hits.length);
+  hits = photo.hits.length;
+  console.log(hits);
+  hits += hits;
+  console.log(hits);
   console.log(photo.totalHits);
 }
 
